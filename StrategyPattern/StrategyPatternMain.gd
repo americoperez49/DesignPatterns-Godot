6276fbaca07f2d0d524e45a478a7cca11e3d4f9e
Americo Perez <americoperez49@gmail.com>
2024-07-10 16:10:49 -0500

    added label and back button to main scene

--- StrategyPattern/StrategyPatternMain.gd
+++ StrategyPattern/StrategyPatternMain.gd
@@ -1,10 +1,14 @@
 extends Node
-const MallardDuck = preload("res://StrategyPattern/SubClasses/Ducks/MallardDuck.gd")
-const RubberDuck = preload("res://StrategyPattern/SubClasses/Ducks/RubberDuck.gd")
-const WoodenDuck = preload("res://StrategyPattern/SubClasses/Ducks/WoodenDuck.gd")
+const MallardDuck = preload("res://StrategyPattern/ConcreteClasses/Ducks/MallardDuck.gd")
+const RubberDuck = preload("res://StrategyPattern/ConcreteClasses/Ducks/RubberDuck.gd")
+const WoodenDuck = preload("res://StrategyPattern/ConcreteClasses/Ducks/WoodenDuck.gd")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	add_child(MallardDuck.new())
 	add_child(RubberDuck.new())
 	add_child(WoodenDuck.new())
+
+
+func _on_back_button_pressed() -> void:
+	pass # Replace with function body.
